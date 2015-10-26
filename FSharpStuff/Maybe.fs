63dbef2d0117c261404fdb878359cba92dc7aa3d
martin bailey <martin.bailey@gmail.com>
2015-10-26 08:52:33 +0000

    stuff

---
+++ FSharpStuff/Maybe.fs
@@ -0,0 +1,25 @@
+﻿module Maybe
+
+
+//type MaybeMonad() =
+//  member t.Bind(m,f) =
+//    match m with
+//    | Some v -> f v
+//    | None -> None
+//  member t.Return v = Some v
+
+type MaybeMonad() =
+  member t.Bind(m,f) = Option.bind f m
+  member t.Return v = Some v
+
+let maybe = MaybeMonad()
+
+let (>>=) m f = Option.bind f m
+
+let log a b = System.Console.WriteLine(sprintf "-Log: %A (%A)" a b)
+
+let foo x    = try 2*x |> Some with ex -> log x ex; None
+let bar x    = try 2+x |> Some with ex -> log x ex; None
+let foobar x = try 2/x |> Some with ex -> log x ex; None
+
+2 |> Some >>= foo >>= bar >>= foobar
--- FSharpStuff/Program.fs
+++ FSharpStuff/Program.fs
@@ -1,41 +1,22 @@
 ﻿module Program
 open System
-open DataStructures
-open Curry
-open Operators
-open Play
-open PatternMatching
 open System.Configuration
 open FSharp.Configuration
-open Bind
+//open DataStructures
+//open Curry
+//open Operators
+//open Play
+//open PatternMatching
+//open Bind
+open Maybe
 
 [<EntryPoint>]
 let main argv = 
     printfn "Hi"
 
+   
 
-    //Play.printProduct
-
-    //printRating shitBook
-
-    //printPowerup powerup
-
-    //printCurry1
-
-    //printIncrement
-
-    //loop
-
-    //printOperators
-    
-   //printIncrement
-
-    //printPattern
-
-
-    //WorldBankProvider.PrintWorldBank
-
-    Bind.p1
+    let x = Maybe.foobar 0
 
     Console.ReadLine() |> ignore
     0 
