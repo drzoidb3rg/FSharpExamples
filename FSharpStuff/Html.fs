c6397ab1bcf78952427ef020ca76e467cbe139bb
martin bailey <martin.bailey@gmail.com>
2015-10-28 15:39:09 +0000

    html

--- FSharpStuff/Html.fs
+++ FSharpStuff/Html.fs
@@ -13,8 +13,18 @@ let doc =
                <img src="myimg.jpg">
                <table title="table" id="results">
                    <tr><th class="ai">Column 1</th><th>Column 2</th></tr>
-                   <tr><td class="ai"><span>int 1</span><span>int 2</span></td><td>yes</td></tr>
-                   <tr><td class="ai"><span>int 1</span><span>int 2</span></td><td>yes</td></tr>
+                   <tr>
+                     <td xmlns="http://www.w3.org/1999/xhtml" class="_index">
+					    <a class="uri" href="/ovidws/resultsets/emef/nlp/pain/2">2</a>
+				     </td>
+                     <td class="ai"><span>int 1</span><span>int 2</span></td><td class="al"><span>name one</span><span>name two</span></td>
+                   </tr>
+                   <tr>
+                     <td xmlns="http://www.w3.org/1999/xhtml" class="_index">
+					    <a class="uri" href="/ovidws/resultsets/emef/nlp/pain/2">2</a>
+				     </td>
+                     <td class="ai"><span>int 1</span><span>int 2</span></td><td class="al"><span>name one</span><span>name two</span></td>
+                   </tr>
                </table>
            </body>
        </html>""" 
@@ -35,33 +45,63 @@ type HtmlHelper () =
   static member ById n id =
     n |> HtmlNode.descendants false (HtmlNode.hasId id) |> Seq.tryPick Some 
 
+  static member TRs n =
+    n |> HtmlNode.descendants false (HtmlNode.hasName "tr") 
+
+  static member ByClass n c =
+    n |> HtmlNode.descendants false (HtmlNode.hasClass c) 
+
   static member TDByClass n c =
     getHtmlNode n "td" c
 
+  static member AByClass n c =
+    getHtmlNode n "a" c
 
 
 type OvidAuthorData  =
    {
+     Index : string
      AuthorInitials : string list option
+     AuthorSurname : string list option
    }  
    static member from (x:HtmlNode) =
      let resultNode = HtmlHelper.ById x "results"
 
-     if resultNode = None then None else
-     
-     let initialPred node = HtmlHelper.TDByClass node "ai"
 
-     let initials = resultNode.Value
-                            |> HtmlNode.descendants false initialPred 
+     //row >>= index >>= index number >>= build author record
+     let anchorClass n node = HtmlHelper.AByClass node n
+
+     let index row = row |> HtmlNode.descendants false (anchorClass "uri") 
+                         |> Seq.tryPick Some
+
+
+     if resultNode = None then None else
+
+     let tdClass n node = HtmlHelper.TDByClass node n
+
+
+     let spannie p = resultNode.Value
+                            |> HtmlNode.descendants false p
                             |> Seq.collect(HtmlNode.descendants false (HtmlNode.hasName "span"))
                             |> Seq.map(HtmlNode.innerText)
                             |> Seq.toList 
                             |> optionalList
 
-     Some {AuthorInitials = initials}
+     let spannie2 n p = n |> HtmlNode.descendants false p
+                          |> Seq.collect(HtmlNode.descendants false (HtmlNode.hasName "span"))
+                          |> Seq.map(HtmlNode.innerText)
+                          |> Seq.toList 
+                          |> optionalList
 
-                                         
-                
+      //need to get list of rows, pass each into spannie
+     let rows = resultNode.Value |> HtmlHelper.TRs 
+                                 |> Seq.map(fun n -> index n)
+                                 |> Seq.toList
+
+
+     Some {Index = ""; AuthorInitials = tdClass "ai" |> spannie; AuthorSurname = tdClass "al" |> spannie}
+
+                                        
 let results = doc |> OvidAuthorData.from
 
 let answer = results
