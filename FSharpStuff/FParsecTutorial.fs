3cb746fa37ded068b14f842993e105652b023965
martin bailey <martin.bailey@gmail.com>
2016-01-06 14:50:44 +0000

    json parser

---
+++ FSharpStuff/FParsecTutorial.fs
@@ -0,0 +1,110 @@
+﻿module FParsecTutorial 
+open FParsec
+
+
+
+let test p str =
+    match run p str with
+    | Success(result, _, _)   -> printfn "Success: %A" result
+    | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
+
+//test pfloat "1.25"
+
+let str s = pstring s
+let floatBetweenBrackets = str "[" >>. pfloat .>> str "]"
+
+//test floatBetweenBrackets "[1.0]"
+
+//let betweenStrings s1 s2 p = str s1 >>. p .>> str s2
+
+let between pBegin pEnd p  = pBegin >>. p .>> pEnd
+let betweenStrings s1 s2 p = p |> between (str s1) (str s2)
+
+test (many floatBetweenBrackets) ""
+
+test (many floatBetweenBrackets) "[2][3][4]"
+
+//many1 : at least one element
+
+test (many1 (floatBetweenBrackets <?> "float between brackets")) "(1)"
+
+
+let floatList = str "[" >>. sepBy pfloat (str ",") .>> str "]"
+
+test floatList "[1,2,3]"
+
+let ws = spaces
+
+let str_ws s = pstring s .>> ws
+
+let float_ws = pfloat .>> ws
+
+let numberList = str_ws "[" >>. sepBy float_ws (str_ws ",") .>> str_ws "]"
+
+test numberList @"[ 1 ,
+                    2 ] "
+
+
+let numberListFile = ws >>. numberList //.>> eof
+
+test numberListFile " [5, 6, 7] [8]"
+
+let identifier =
+  let isIdentifierFirstChar c = isLetter c || c = '_'
+  let isIdentifierChar c = isLetter c || isDigit c || c = '_'
+  
+  many1Satisfy2L isIdentifierFirstChar isIdentifierChar "identifier monkey"
+    .>> ws 
+
+test identifier "test1="
+
+let unescape c = match c with
+                     | 'n' -> '\n'
+                     | 'r' -> '\r'
+                     | 't' -> '\t'
+                     | c   -> c
+
+let stringLiteral =
+    let normalChar = satisfy (fun c -> c <> '\\' && c <> '"')
+
+    let escapedChar = pstring "\\" >>. (anyOf "\\nrt\"" |>> unescape)
+    between (pstring "\"") (pstring "\"")
+            (manyChars (normalChar <|> escapedChar))
+
+test stringLiteral "\"a \nb c\""
+
+//let stringLiteral2 =
+//    let normalCharSnippet = many1Satisfy (fun c -> c <> '\\' && c <> '"')
+//    let escapedChar = pstring "\\" >>. (anyOf "\\nrt\"" |>> function
+//                                                            | 'n' -> "\n"
+//                                                            | 'r' -> "\r"
+//                                                            | 't' -> "\t"
+//                                                            | c   -> string c)
+//    between (pstring "\"") (pstring "\"")
+//            (manyStrings (normalCharSnippet <|> escapedChar))
+
+
+let product = pipe2 float_ws (str_ws "*" >>. float_ws) (fun x y -> x * y)
+
+test product "3 * 5"
+
+type StringConstant = StringConstant of string * string
+
+let stringConstant = pipe3 identifier (str_ws "=") stringLiteral
+                           (fun id _ str -> StringConstant(id, str))
+
+test stringConstant "myString = \"stringValue\""
+
+test (float_ws .>>. (str_ws "," >>. float_ws)) "123,456"  
+
+let boolean =  (stringReturn "true"  true)
+              <|> (stringReturn "false" false)    
+ 
+test boolean "false"            
+
+
+type UserState = unit // doesn't have to be unit, of course
+type Parser<'t> = Parser<'t, UserState>
+
+let p : Parser<_> = pstring "test"
+
---
+++ FSharpStuff/JsonParser.fs
@@ -0,0 +1,81 @@
+﻿module JsonParser
+
+open FParsec
+
+type UserState = unit // doesn't have to be unit, of course
+type Parser<'t> = Parser<'t, UserState>
+let str s = pstring s
+
+let test p str =
+    match run p str with
+    | Success(result, _, _)   -> printfn "Success: %A" result
+    | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
+
+
+type Json = JString of string
+          | JNumber of float
+          | JBool   of bool
+          | JNull
+          | JList   of Json list
+          | JObject of Map<string, Json>
+
+
+let jnull: Parser<_>  = stringReturn "null" JNull
+
+let jbool: Parser<_>  = (stringReturn "true"  (JBool true))
+                       <|> (stringReturn "false" (JBool false))
+
+let jnumber:Parser<_>  = pfloat |>> JNumber
+
+let stringLiteral:Parser<_>  =
+    let escape =  anyOf "\"\\/bfnrt"
+                  |>> function
+                      | 'b' -> "\b"
+                      | 'f' -> "\u000C"
+                      | 'n' -> "\n"
+                      | 'r' -> "\r"
+                      | 't' -> "\t"
+                      | c   -> string c // every other char is mapped to itself
+
+    let unicodeEscape =
+        /// converts a hex char ([0-9a-fA-F]) to its integer number (0-15)
+        let hex2int c = (int c &&& 15) + (int c >>> 6)*9
+
+        str "u" >>. pipe4 hex hex hex hex (fun h3 h2 h1 h0 ->
+            (hex2int h3)*4096 + (hex2int h2)*256 + (hex2int h1)*16 + hex2int h0
+            |> char |> string
+        )
+
+    let escapedCharSnippet = str "\\" >>. (escape <|> unicodeEscape)
+    let normalCharSnippet  = manySatisfy (fun c -> c <> '"' && c <> '\\')
+
+    between (str "\"") (str "\"")
+            (stringsSepBy normalCharSnippet escapedCharSnippet)
+
+
+let jvalue, jvalueRef = createParserForwardedToRef<Json, unit>()
+
+let jstring = stringLiteral |>> JString
+
+let ws = spaces
+
+let listBetweenStrings sOpen sClose pElement f =
+    between (str sOpen) (str sClose)
+            (ws >>. sepBy (pElement .>> ws) (str "," >>. ws) |>> f)
+
+
+let jlist = listBetweenStrings "[" "]" jvalue JList
+
+let keyValue = stringLiteral .>>. (ws >>. str ":" >>. ws >>. jvalue)
+
+
+let jobject = listBetweenStrings "{" "}" keyValue (Map.ofList >> JObject)
+
+do jvalueRef := choice [jobject
+                        jlist
+                        jstring
+                        jnumber
+                        jbool
+                        jnull]
+
+let json = ws >>. jvalue .>> ws .>> eof
